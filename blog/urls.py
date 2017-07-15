49f74dd823b31e64422a0c88f00951b06bb61449
Chinseok Lee <allieuslee@gmail.com>
2017-07-15 15:41:10 +0900

    blog.views.mysum 구현 (인자 1개)

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^sum/(?P<x>\d+)/$', views.mysum),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
+
+def mysum(request, x):
+    return HttpResponse(x)
 
