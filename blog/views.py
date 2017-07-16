867bc5d46f8b80f1fb34270be147e3333bd3f2df
Chinseok Lee <allieuslee@gmail.com>
2017-07-16 15:45:17 +0900

    blog.views.post_list 뷰에서 Post 목록 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
 
 def mysum(request, numbers):
     # "10/20/30/40/50/60".split("/")
