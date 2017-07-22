552b3bbbfa51830d72847ae4b63f1dab6846ce58
Chinseok Lee <allieuslee@gmail.com>
2017-07-22 16:29:41 +0900

    blog post_list/post_detail URL Reverse 적용

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^(?P<pk>\d+)/$', views.post_detail),
-    url(r'^sum/(?P<numbers>[0-9/]+)/$', views.mysum),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^sum/(?P<numbers>[0-9/]+)/$', views.mysum, name='mysum'),
 ]
 
