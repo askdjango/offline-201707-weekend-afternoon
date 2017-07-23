49b31520819e82ff28d77a00500f8afa9b309518
Chinseok Lee <allieuslee@gmail.com>
2017-07-23 14:32:19 +0900

    blog.models.Post 모델에 help_text필드 추가

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Post(models.Model):
     author = models.CharField(max_length=20)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, help_text='이름 3글자만 넣어주세요.')
+    content = models.TextField(help_text='Markdown 문법을 지원합니다.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
