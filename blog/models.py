d516956cd01aeba2013c4797a2aadec5e50b85db
Chinseok Lee <allieuslee@gmail.com>
2017-07-23 15:42:00 +0900

    blog.models.Post 모델의 title필드에 커스텀 validator 적용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,18 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 
 
+def min_length_3_validator(value):
+    if len(value) < 3:
+        raise ValidationError('3글자 이상 입력해주세요.')
+
+
 class Post(models.Model):
     author = models.CharField(max_length=20)
-    title = models.CharField(max_length=100, help_text='이름 3글자만 넣어주세요.')
+    title = models.CharField(max_length=100,
+            validators=[min_length_3_validator],
+            help_text='이름 3글자만 넣어주세요.')
     content = models.TextField(help_text='Markdown 문법을 지원합니다.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
