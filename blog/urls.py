18b658a3c2264091a79b5853781696379edbb9a8
Chinseok Lee <allieuslee@gmail.com>
2017-07-23 14:41:20 +0900

    blog post_new/post_edit CBV 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from . import views
+from . import views_cbv
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^new/$', views.post_new, name='post_new'),
+    url(r'^new/$', views_cbv.post_new, name='post_new'),
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^(?P<pk>\d+)/edit/$', views_cbv.post_edit, name='post_edit'),
     url(r'^sum/(?P<numbers>[0-9/]+)/$', views.mysum, name='mysum'),
 ]
 
---
+++ blog/views_cbv.py
@@ -0,0 +1,8 @@
+from django.views.generic import CreateView, UpdateView
+from .models import Post
+from .forms import PostForm
+
+post_new = CreateView.as_view(model=Post, form_class=PostForm, success_url='/weblog/')
+
+post_edit = UpdateView.as_view(model=Post, form_class=PostForm, success_url='/weblog/')
+
