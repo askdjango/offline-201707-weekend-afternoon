26e89a750195b6ddb1da507cd5695f1c34269bb2
Chinseok Lee <allieuslee@gmail.com>
2017-07-29 14:34:29 +0900

    media 파일 서빙 지원

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -22,6 +23,8 @@ urlpatterns = [
     url(r'^weblog/', include('blog.urls', namespace='blog')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
