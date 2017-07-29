64c444a37156620dd5b9e0b2e8feebaec285c585
Chinseok Lee <allieuslee@gmail.com>
2017-07-29 17:24:05 +0900

    회원가입 커스텀

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
         # fields = ('username', 'email')
+
+
+def answer_validator(value):
+    if value != 6:
+        raise forms.ValidationError('땡~!!!')
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(help_text='3+3=?', validators=[answer_validator])
 
