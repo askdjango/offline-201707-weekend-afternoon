a726a8222394b6da483605ad3107e7c7f28ee939
Chinseok Lee <allieuslee@gmail.com>
2017-07-29 17:26:18 +0900

    로그인 answer필드에 대한 유효성 검사를 clean_ 에서 수행

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,10 +8,18 @@ class SignupForm(UserCreationForm):
         # fields = ('username', 'email')
 
 
+'''
 def answer_validator(value):
     if value != 6:
         raise forms.ValidationError('땡~!!!')
+'''
 
 class LoginForm(AuthenticationForm):
-    answer = forms.IntegerField(help_text='3+3=?', validators=[answer_validator])
+    answer = forms.IntegerField(help_text='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None)
+        if answer != 6:
+            raise forms.ValidationError('땡~!!!')
+        return answer
 
